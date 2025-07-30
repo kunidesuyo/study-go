0b4ece6b1511d3868a2548a3478ca1c8c181d1b0
kuni <kunidesuyo1234@gmail.com>
2025-07-30 23:42:34 +0900

    2-2

--- lesson.go
+++ lesson.go
@@ -2,15 +2,10 @@ package main
 
 import "fmt"
 
-func foo(params ...int) {
-	fmt.Println(len(params), params)
-	for _, param := range params {
-		fmt.Println(param)
-	}
-}
-
 func main() {
-	s := make([]int, 3)
-	fmt.Println(s)
-	foo(s...)
+	l := []string{"python", "go", "java"}
+
+	for i, v := range l {
+		fmt.Println(i, v)
+	}
 }
