fc554cc1be6bc82384dac7bd7ca6d23c3dc548c8
kuni <kunidesuyo1234@gmail.com>
2025-07-31 19:35:22 +0900

    5-1

--- lesson.go
+++ lesson.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
-	// "io"
-	"errors"
-	"strings"
+	"sync"
 )
 
-func main() {
-	r := strings.NewReader("Hello, Reader!")
-
-	b := make([]byte, 8)
-	e := errors.New("EOF")
-
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == e {
-			break
-		}
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
 	}
+}
+
+func goroutine(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go goroutine("world", &wg)
+	normal("hello")
+	// time.Sleep(2000 * time.Millisecond)
+	wg.Wait()
 }
