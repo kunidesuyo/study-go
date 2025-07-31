813e853918d600bbc118057900f50786b36084cc
kuni <kunidesuyo1234@gmail.com>
2025-07-31 18:23:20 +0900

    3-3

--- lesson.go
+++ lesson.go
@@ -7,23 +7,21 @@ import (
 	// "io"
 )
 
-// func one(x *int) {
-// 	*x = 1
-// }
+type Vertex struct {
+	X, Y int
+	S string
+}
+
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	(*v).X = 1000
+}
 
 func main() {
-	// s := make([]int, 0)
-	// fmt.Printf("%T\n", s)
-
-	// m := make(map[string]int)
-	// fmt.Printf("%T\n", m)
-
-	// var p *int = new(int)
-	// fmt.Printf("%T\n", p)
-
-	ch := make(chan int)
-	fmt.Printf("%T\n", ch)
-
-	var st = new(struct{})
-	fmt.Printf("%T\n", st)
+	v2 := &Vertex{X: 1, Y: 2, S: "test"}
+	changeVertex2(v2)
+	fmt.Println(v2)
 }
