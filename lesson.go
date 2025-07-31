7cf03503017451a7d69bf0271e106e97eb7218bf
kuni <kunidesuyo1234@gmail.com>
2025-07-31 18:07:59 +0900

    3-2

--- lesson.go
+++ lesson.go
@@ -7,14 +7,23 @@ import (
 	// "io"
 )
 
-func one(x *int) {
-	*x = 1
-}
+// func one(x *int) {
+// 	*x = 1
+// }
 
 func main() {
-	var n int = 100
-	one(&n)
-	fmt.Println(n)
-	fmt.Println(&n)
-	fmt.Println(&*&n)
+	// s := make([]int, 0)
+	// fmt.Printf("%T\n", s)
+
+	// m := make(map[string]int)
+	// fmt.Printf("%T\n", m)
+
+	// var p *int = new(int)
+	// fmt.Printf("%T\n", p)
+
+	ch := make(chan int)
+	fmt.Printf("%T\n", ch)
+
+	var st = new(struct{})
+	fmt.Printf("%T\n", st)
 }
