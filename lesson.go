e8bc4ea3789f6e24fd3c8c40e25f6dd2bd360afd
kuni <kunidesuyo1234@gmail.com>
2025-07-31 18:46:24 +0900

    4-2

--- lesson.go
+++ lesson.go
@@ -7,13 +7,41 @@ import (
 	// "io"
 )
 
-type Myint int
+type Vertex struct {
+	x, y int
+}
 
-func (i Myint) Double() Myint {
-	return i * 2
+func (v Vertex) Area() int {
+	return v.x * v.y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+type Vertex3D struct {
+	Vertex
+	z int
+}
+
+func (v Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
 }
 
 func main() {
-	myInt := Myint(10)
-	fmt.Println(myInt.Double())
+	v := New(3, 4, 5)
+	v.Scale3D(10)
+	// fmt.Println(v.Area())
+	fmt.Println(v.Area3D())
 }
