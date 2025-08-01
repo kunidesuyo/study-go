1e755e29f890be7b9ff24752bb1d1ae9ca9a1a2b
kuni <kunidesuyo1234@gmail.com>
2025-08-01 23:53:19 +0900

    5-2途中

--- lesson.go
+++ lesson.go
@@ -20,11 +20,33 @@ func goroutine(s string, wg *sync.WaitGroup) {
 	}
 }
 
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func goroutine2(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go goroutine("world", &wg)
-	normal("hello")
-	// time.Sleep(2000 * time.Millisecond)
-	wg.Wait()
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+	x := <-ch
+	fmt.Println(x)
+	
+	fmt.Println(len(ch))
+
+	ch <- 300
+	fmt.Println(len(ch))	
 }
