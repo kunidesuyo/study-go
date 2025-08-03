6313c629b52899ffe5772946b3f00c310033d430
kuni <kunidesuyo1234@gmail.com>
2025-08-03 18:10:56 +0900

    5-7

--- lesson.go
+++ lesson.go
@@ -2,24 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+type Counter struct {
+	v map[string]int
+	mux sync.Mutex
+}
+
+func (c *Counter) Inc(key string) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
 func main() {
-	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Millisecond)
-OuterLoop:
-	for {
-		select {
-		case <-tick:
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("BOOM")
-			break OuterLoop
-		default:
-			fmt.Println("    .")
-			time.Sleep(50 * time.Millisecond)
+	c := Counter{v: make(map[string]int)}
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("Key")
 		}
-	}
-	fmt.Println("########################")
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("key")
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	fmt.Println(c.v, c.Value("key"))
 }
