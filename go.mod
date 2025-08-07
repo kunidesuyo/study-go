661bcbf4fae3c9f61da2c5a9e3b3d5e53479c385
kuni <kunidesuyo1234@gmail.com>
2025-08-07 09:59:13 +0900

    6-4

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module awesomeProject
 
 go 1.24.5
+
+require (
+	github.com/markcheno/go-quote v0.0.0-20250404014651-c94d8a57545f // indirect
+	github.com/markcheno/go-talib v0.0.0-20250114000313-ec55a20c902f // indirect
+)
--- main.go
+++ main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"fmt"
 
-	"awesomeProject/mylib"
-	"awesomeProject/mylib/under"
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
 )
 
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	fmt.Println(mylib.Average(s))
-
-	mylib.Say()
-	under.Hello()
-	person := mylib.Person{Name: "Mike", Age: 20}
-	fmt.Println(person)
-
-	fmt.Println(mylib.Public)
+	spy, _ := quote.NewQuoteFromYahoo("SPY", "2016-01-01", "2016-04-01", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
 }
